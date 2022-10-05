2172a5b9ce7e5e9a90c5ec55c0c51968524d2d90
Abinash Karmakar <codeAbinash@gmail.com>
2022-10-05 09:43:52 +0530

    Fixed pronounce

--- js/script.js
+++ js/script.js
@@ -229,7 +229,7 @@ function showData(data) {
     }
 
     function setClickToFav(wordData) {
-        const favElements = document.querySelectorAll('#results .result .word >.actions img.fav-button')
+        const favElements = document.querySelectorAll('#search-results .result .word >.actions img.fav-button')
         let countFav = 0
         favElements.forEach(elem => {
             const word = wordData[countFav].word
@@ -242,12 +242,13 @@ function showData(data) {
     }
 
     function setAudio(data) {
-        const audioElements = document.querySelectorAll('#results .result .word >.actions img.voice-button')
+        const audioElements = document.querySelectorAll('#search-results .result .word >.actions img.voice-button')
+        console.log(audioElements)
         let audioCount = 0
         audioElements.forEach(elem => {
             if (data[audioCount].phonetics.length) {
                 const audio = new Audio(getAudioSource(data[audioCount].phonetics))
-                elem.onclick = () => { audio.play() }
+                elem.onclick = () => { audio.play(); console.log('Hello Abinash') }
             }
             audioCount++
         })
